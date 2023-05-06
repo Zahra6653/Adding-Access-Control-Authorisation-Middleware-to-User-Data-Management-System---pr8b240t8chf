ce5b59515a78a3e79f3b412560489699ffb07c05
Zahra6653 <zahrasaktiwala@gmail.com>
2023-05-06 11:23:58 +0530

    First Commit

--- middlewares/grantAccessTo.js
+++ middlewares/grantAccessTo.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'newtonSchool';
 
 /*
-Write a middleware function that checks if the user's role matches with the one given passed in the middleware as an array of roles. The token should be passed in the authorization header, the middle should return a 403 status code if the user's role is not present in the roles array and a 401 status code if the token is invalid or missing.
+Write a middleware function that checks if the user's role matches with the one given passed in the middleware as an array of roles. 
+The token should be passed in the authorization header, the middle should return a 403 status code if the user's role is not present in 
+the roles array and a 401 status code if the token is invalid or missing.
 
 Possible Cases: 
 1. Token is missing : { message: 'Authentication failed: Missing token.', status: "Error"}
@@ -15,7 +17,22 @@ function grantAccessTo(roles) {
   return function (req, res, next) {
     //Write your code here;
     try{
-    } catch (err) {
+      const {role}=req.body;
+      const token = req.headers.authorization;
+      
+      if(!roles.includes(role)){
+        res.status(403).json({
+          status: "error",
+          message: "Access Denied"
+        })
+      }
+      else if(!token){
+        res.status(401).json({ message: 'Authentication failed: Missing token.', status: "Error" });
+      }
+      else{
+        next();
+      }
+        } catch (err) {
       return res.status(401).json({ message: 'Authentication failed: Invalid token.', status: "Error" });
     }
   }
